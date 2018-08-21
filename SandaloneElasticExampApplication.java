490b8685fd9f28b1b983079556a5b645e3c1a886
Robesz <kvrobert@gmail.com>
2018-08-21 21:58:28 +0200

    Using ES Transport client in Spring

---
+++ SandaloneElasticExampApplication.java
@@ -0,0 +1,12 @@
+package com.kvrobert.elastic.sandaloneelasticexamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandaloneElasticExampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandaloneElasticExampApplication.class, args);
+    }
+}
---
+++ controllers/UsersResource.java
@@ -0,0 +1,93 @@
+package com.kvrobert.elastic.sandaloneelasticexamp.controllers;
+
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequestBuilder;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Date;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+
+@RestController
+@RequestMapping("/rest/users")
+public class UsersResource {
+
+    private TransportClient client;
+
+    public UsersResource() throws UnknownHostException {
+         this.client = new PreBuiltTransportClient(Settings.EMPTY)
+                .addTransportAddress(new TransportAddress(InetAddress.getByName("localhost"), 9300));
+    }
+
+    @GetMapping("/insert/{id}")
+    public String insert(@PathVariable final String id) throws IOException {
+        IndexResponse response = client.prepareIndex("employee", "id", id)
+                .setSource(jsonBuilder()
+                        .startObject()
+                        .field("name", "Robesz")
+                        .field("salary", 1200)
+                        .field("teamName", "Development")
+                        .endObject()
+                )
+                .get();
+        return  response.getResult().toString();
+    }
+
+    @GetMapping("/view/{id}")
+    public Map<String, Object> view(@PathVariable final String id){
+        GetResponse getResponse = client.prepareGet("employee", "id", id ).get();
+        System.out.println(getResponse.getSource());
+        return getResponse.getSource();
+    }
+
+    @GetMapping("/update/{id}")
+    public String update(@PathVariable final String id) throws IOException {
+
+        UpdateRequest updateRequest = new UpdateRequest();
+        updateRequest.index("employee")
+                .type("id")
+                .id(id)
+                .doc(jsonBuilder()
+                    .startObject()
+                    .field("gender", "male")
+                    .endObject());
+
+        try {
+            UpdateResponse updateResponse = client.update(updateRequest).get();
+            System.out.println(updateResponse.status());
+            return updateResponse.status().toString();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            System.out.println(e);
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+            System.out.println(e);
+        }
+
+        return "Exception";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable final String id){
+        DeleteResponse deleteResponse = client.prepareDelete("employee", "id", id).get();
+        System.out.println(deleteResponse.getResult().toString());
+
+        return deleteResponse.getResult().toString(); 
+    }
+}
